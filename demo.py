0ab7e69d0655b6b40d2f3426e14a2885555ce27b
Rick Cottle <richard.cottle@honeywell.com>
2019-08-12 07:48:23 -0700

    .

--- demo.py
+++ demo.py
@@ -4,10 +4,16 @@ from opensky_api import OpenSkyApi
 
 api = OpenSkyApi()
 while True:
-    s = api.get_states(bbox=(33.1951,33.3397,-111.8169,-111.6440))
-    for s1 in s.states:
-        print("(%r, %r, %r, %r, %r)" % \
-               (s1.callsign, s1.longitude, s1.latitude,\
-                s1.baro_altitude, s1.velocity))
-    print("--------\n")
-    time.sleep(30)
+#	s = api.get_states(bbox=(33.1951,33.3397,-111.8169,-111.6440)) home
+	s = api.get_states(bbox=(33.0004524786153, 33.8678875213847, -112.531391304461, -111.491948695539))
+	for s1 in s.states:
+		callsign = (s1.callsign + " "*8)[:7]
+		f_altitude = float(s1.baro_altitude) * 3.2804
+		t_altitude = "%6.1f" % f_altitude
+		f_velocity = float(s1.velocity) * 2.23694
+		t_velocity = "%4.1f" % f_velocity
+		print("(%r, %r, %r, %r, %r)" % \
+			(s1.callsign, s1.longitude, s1.latitude,\
+			t_altitude, t_velocity))
+	print("--------\n\n")
+	time.sleep(30)
